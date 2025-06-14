46b223ff873291ced53ecba6ddab10743845c6e7
slava5879 <lan1547@ukr.net>
2025-06-14 12:58:14 +0300

    add Example class

--- src/main/java/org.example/Example.java
+++ src/main/java/org.example/Example.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Example {
+class Example {
     public static void main (String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/org.example/IfDemo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class IfDemo {
+    public static void main(String[] args) {
+        int a,b;
+        a = 2;
+        b = 3;
+
+        if (a < b) System.out.println("a is less than b");
+        System.out.println("Finish");
+    }
+}
