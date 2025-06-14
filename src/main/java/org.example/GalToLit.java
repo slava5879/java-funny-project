faa5d4dddcfcf0305ae60b2d948997a50b591778
slava5879 <lan1547@ukr.net>
2025-06-14 14:51:32 +0300

    added new examples

--- src/main/java/org.example/GalToLit.java
+++ src/main/java/org.example/GalToLit.java
@@ -2,12 +2,19 @@ package org.example;
 
 public class GalToLit {
     public static void main(String[] args) {
-        double gallons;
-        double liters;
+        double gallons, liters;
+        int counter;
 
-        gallons = 10;
-        liters = gallons * 3.7854;
-        System.out.println(gallons + " gallons are equal to "
-                           + liters + " liters.");
+        counter = 0;
+        for (gallons = 1; gallons <= 100; gallons++) {
+            liters = gallons * 3.7854;
+            System.out.println(gallons + " gallons are equal to " + liters + " liters.");
+            counter++;
+
+            if (counter == 10) {
+                System.out.println();
+                counter = 0;
+            }
+        }
     }
 }
